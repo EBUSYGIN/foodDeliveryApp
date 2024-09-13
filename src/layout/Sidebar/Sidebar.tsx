b4c5da2dc947b04559e2ad8a73e730ba0b7a98b3
EBUSYGIN <e.busygin2003@gmail.com>
2024-09-13 15:12:52 +0300

    make profile load after authorization

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,17 +1,29 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../Redux/store';
+import { userActions } from '../../Redux/user.slice';
 
 function Sidebar() {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const profile = useSelector((s: RootState) => s.user.profile);
+
+  const logout = () => {
+    dispatch(userActions.removeJwt());
+    navigate('/auth/login');
+  };
+
   return (
     <div className={styles.layout}>
       <div className={styles.sidebarContainer}>
         <nav className={styles.sidebar}>
           <div className={styles.user}>
             <img className={styles.avatar} src='/avatar.png' alt='Аватар' />
-            <div className={styles.name}>Имя фамилия</div>
-            <div className={styles.email}>@gmail.com</div>
+            <div className={styles.name}>{profile?.name}</div>
+            <div className={styles.email}>{profile?.email}</div>
           </div>
           <div className={styles.menu}>
             <NavLink
@@ -37,7 +49,7 @@ function Sidebar() {
               Корзина
             </NavLink>
           </div>
-          <Button appereance='small' className={styles.exit}>
+          <Button appereance='small' className={styles.exit} onClick={logout}>
             <img src='exit-icon.svg' alt='Иконка выхода' />
             Выйти
           </Button>
